7e57d5517edeb503aaa2d23a7886a2a7edf84ab6
GregVillongco <glvillongco@gmail.com>
2026-09-04 14:18:48 -0700

    Add: missing-number - submission-5

---
+++ Data Structures & Algorithms/missing-number/submission-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int len = nums.size();
+
+        int t1 = 0;
+        for(auto n : nums){
+            t1 += n;
+        }
+
+        int t2 = (len * (len + 1)) / 2;
+
+        return t2 - t1;
+        
+    }
+};
