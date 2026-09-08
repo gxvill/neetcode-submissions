d0f6d1162826a97fbb18ea60cd06108de4b44dbb
GregVillongco <glvillongco@gmail.com>
2026-09-08 14:00:06 -0700

    Add: string-matching-in-an-array - submission-1

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> ret;
+        sort(words.begin(), words.end(), [](const string& a, const string& b){
+                return a.length() < b.length();
+                });
+
+        for(int i = 0; i < words.size() - 1; i ++){
+            for(int j = i + 1; j < words.size(); j++){
+                if(words[j].find(words[i]) != string::npos){
+                    ret.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+
+        return ret;
+    }
+};
+
+
