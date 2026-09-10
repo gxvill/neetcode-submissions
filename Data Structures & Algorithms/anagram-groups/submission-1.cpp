f385ecf30ca6e9009549d3dbab76fdc5630350b9
GregVillongco <glvillongco@gmail.com>
2026-09-10 01:19:03 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+
+        for(auto& s : strs){
+            string key = s;
+            sort(key.begin(), key.end());
+            groups[key].push_back(s);
+        }
+
+        vector<vector<string>> anagrams;
+        for(auto& [key, group] : groups){
+            anagrams.push_back(group);
+        }
+        return anagrams;
+    }
+};
+
