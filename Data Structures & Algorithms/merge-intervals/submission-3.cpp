ce0fd39635e664c386885b7d6d4e82b436bd0110
GregVillongco <glvillongco@gmail.com>
2026-09-24 19:17:30 -0700

    Add: merge-intervals - submission-3

---
+++ Data Structures & Algorithms/merge-intervals/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size() == 1) return intervals;
+        
+        sort(intervals.begin(), intervals.end());
+
+        for(int i = 0; i < intervals.size() - 1; ){
+            if(intervals[i][1] >= intervals[i+1][0]){
+                intervals[i][1] = max(intervals[i+1][1], intervals[i][1]);
+                intervals.erase(intervals.begin() + i+1);
+            }
+            else{
+                i++;
+            }
+        }
+
+        return intervals;
+    }
+};
+
+
+
+
+
+
