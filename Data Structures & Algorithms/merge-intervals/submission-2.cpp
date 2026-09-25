952ba9e66d5fe849bea3a9d7bcb83036867bb411
GregVillongco <glvillongco@gmail.com>
2026-09-24 19:11:11 -0700

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size() == 1) return intervals;
+        
+        sort(intervals.begin(), intervals.end());
+
+        for(int i = 0; i < intervals.size() - 1; ){
+            if(intervals[i][1] >= intervals[i+1][0]){
+                if(intervals[i+1][1] > intervals[i][1]){
+                    intervals[i][1] = intervals[i+1][1];
+                }
+                intervals.erase(intervals.begin() + i+1);
+            }
+            else{
+                i++;
+            }
+        }
+
+        return intervals;
+    }
+};
+
+
+
+
+
